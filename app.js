9ab18e5065a4c3409c6709e78c0ba373a0b68552
pavithra <pavithra.25cs@licet.ac.in>
2025-02-22 20:23:41 +0530

    Rejection example

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hi Pavithra, Welcome! The content has been modified.'));
+app.get('/', (req, res) => res.send('Hi Pavithra, Welcome! This is an example for rejection.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
